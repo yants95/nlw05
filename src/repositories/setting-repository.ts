964913bc77d6716cf2a5297d9500b20862b8d996
Yan Soares <yts.1995@gmail.com>
2021-04-20 15:01:13 -0300

    chore: add setting-repository

---
+++ src/repositories/setting-repository.ts
@@ -0,0 +1,8 @@
+import { Setting } from '../entities';
+
+import { Repository, EntityRepository } from 'typeorm'
+
+@EntityRepository(Setting)
+export class SettingRepository extends Repository<Setting> {
+
+}
