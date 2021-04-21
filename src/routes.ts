3c9e17061cf8e3ce3b108d1027e2323bcaa7ae64
Yan Soares <yts.1995@gmail.com>
2021-04-21 10:48:19 -0300

    chore: add users and messages routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
-import { SettingsController } from '@/controllers'
+import { SettingsController, UserController, MessageController } from '@/controllers'
 
 import { Router } from 'express'
 
 const routes = Router()
+
 const settingsController = new SettingsController()
+const usersController = new UserController()
+const messageController = new MessageController()
 
 routes.post('/settings', settingsController.create)
+routes.post('/users', usersController.create)
+routes.post('/messages', messageController.create)
 
 export { routes }
