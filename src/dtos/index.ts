08776e1a98122e0794f8cdc6de1c4defc2869edd
Yan Soares <yts.1995@gmail.com>
2021-04-21 09:58:08 -0300

    refactor: remove create-user-dto

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,2 +1,1 @@
 export * from './create-settings-dto'
-export * from './create-user-dto'
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,11 +1,9 @@
-import { CreateUserDTO } from '@/dtos'
 import { UserRepository } from '@/repositories'
 import { User } from '@/entities'
 
 import { getCustomRepository } from 'typeorm'
-
 export class UserService {
-  async create ({ email }: CreateUserDTO): Promise<User> {
+  async create (email: string): Promise<User> {
     const userRepository = getCustomRepository(UserRepository)
 
     const userExisting = await userRepository.findOne({ email })
