fd23b092094aaf5013ea4da609910584ec94c7e5
Yan Soares <yts.1995@gmail.com>
2021-04-21 17:27:48 -0300

    refactor: change to use request.body directly

--- src/controllers/create-message-controller.ts
+++ src/controllers/create-message-controller.ts
@@ -5,15 +5,8 @@ import { Request, Response } from 'express'
 export class CreateMessageController {
   async create (request: Request, response: Response): Promise<Response> {
     try {
-      const { admin_id, user_id, text } = request.body
       const createMessageService = new CreateMessageService()
-
-      const messageParams = {
-        admin_id,
-        user_id,
-        text
-      }
-      const message = await createMessageService.create(messageParams)
+      const message = await createMessageService.create(request.body)
 
       return response.json(message)
     } catch (err) {
