f8a421d6835628feb8f3b2512bae46eea2265d52
Yan Soares <yts.1995@gmail.com>
2021-04-22 12:12:07 -0300

    feat: add new routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,11 @@
-import { CreateSettingController, CreateUserController, CreateMessageController, ListMessagesByUserController } from '@/controllers'
+import {
+  CreateSettingController,
+  CreateUserController,
+  CreateMessageController,
+  ListMessagesByUserController,
+  FindByUsernameController,
+  UpdateSettingsController
+} from '@/controllers'
 
 import { Router } from 'express'
 
@@ -8,8 +15,12 @@ const createSettingController = new CreateSettingController()
 const createUserController = new CreateUserController()
 const createMessageController = new CreateMessageController()
 const listMessagesByUserController = new ListMessagesByUserController()
+const findByUsernameController = new FindByUsernameController()
+const updateSettingsController = new UpdateSettingsController()
 
 routes.post('/settings', createSettingController.create)
+routes.get('/settings/:username', findByUsernameController.find)
+routes.put('/settings/:username', updateSettingsController.update)
 
 routes.post('/users', createUserController.create)
 
